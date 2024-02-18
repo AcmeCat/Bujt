7621b88dd55d4af81da14222ad06bade2577dd8e
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2024-02-18 10:54:44 +0800

    Initial Commit

---
+++ Bujt/BujtApp.swift
@@ -0,0 +1,17 @@
+//
+//  BujtApp.swift
+//  Bujt
+//
+//  Created by Rory Allen on 18/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct BujtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bujt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bujt
+//
+//  Created by Rory Allen on 18/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
